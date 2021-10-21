5641ea02242b29e10bd18a291128bb354abdfb08
unknown <gagahre4@gmail.com>
2021-10-21 14:27:39 +0300

    Some Changes

--- clientApp/src/app/shared/models/Product.ts
+++ clientApp/src/app/shared/models/Product.ts
@@ -1,11 +1,8 @@
-export class Product{
-
-    constructor(
-       public productId: number,
-       public productName: string,
-       public price: number,
-       public quantityInStock: number,
-       public lastModified: Date,
-       public ProductCategoriesId?: number[] 
-    ){}
+export interface Product{
+     productId: number;
+     productName: string;
+     price: number;
+     quantityInStock: number;
+     lastModified: Date;
+     ProductCategoriesId?: number[]; 
 }
